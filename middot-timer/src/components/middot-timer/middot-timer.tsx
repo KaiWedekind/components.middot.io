a141a71b35217a5bf2bfc6cb4b5778eaa89af67b
Kai Wedekind <kai.wedekind@de.ibm.com>
2018-11-09 14:16:34 +0100

    Add middot-timer

---
+++ middot-timer/src/components/middot-timer/middot-timer.tsx
@@ -0,0 +1,12 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'middot-timer',
+  styleUrl: 'middot-timer.css',
+  shadow: true
+})
+export class MiddotTimer {
+  render() {
+    return <div>Middot Timer</div>;
+  }
+}
